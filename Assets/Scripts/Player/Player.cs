0efcbbfeab100805338f5f1afa526f94178ebd66
2000krysztof <2000krzysztof@gmail.com>
2024-04-29 17:58:30 +0100

    Add player satates

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private PlayerBaseState _currentState;
+    public PlayerRunningState runningState;
+    public PlayerIdleState idleState;
+    public PlayerDraggingState draggingState;
+
+
+    [SerializeField] private float moveSpeed = 10f;
+
     void Start()
     {
-        
+        draggingState = new PlayerDraggingState();
+        runningState = new PlayerRunningState();
+        idleState = new PlayerIdleState();
+        switchState(idleState);
     }
 
     void Update()
     {
-        
+       _currentState.update(this); 
     }
+
+    public void switchState(PlayerBaseState state)
+    {
+        state.enter(this);
+        _currentState = state;
+    }
+    
+    
 }
